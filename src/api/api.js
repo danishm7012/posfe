edca503d1a5ca6fbd18d30f86adb3209beef3872
husnain <husnain.tahir@quanrio.com>
2023-01-25 13:00:56 +0500

    server deployed

--- src/api/api.js
+++ src/api/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: "http://localhost:4001/api/"
-    baseURL: "https://heaven-engineering-be.herokuapp.com/api/"
+    baseURL: "http://3.130.132.140/api/"
 })
